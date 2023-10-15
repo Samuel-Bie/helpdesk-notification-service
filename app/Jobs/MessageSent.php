f6225c1bdfc39834bfb13e27c7609438f3fbabd1
samuel-bie <samuel.bie75@gmail.com>
2023-10-15 17:11:35 +0200

    release Basic Service to send notifications

--- app/Jobs/MessageSent.php
+++ app/Jobs/MessageSent.php
@@ -3,29 +3,46 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class MessageSent implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private $data;
     /**
      * Create a new job instance.
+     *
+     * @return void
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
 
     /**
      * Execute the job.
+     *
+     * @return void
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+    	if ($this->data) {
+
+            /**
+             * Here we would add a message sent notification
+             * via email ou push using laravel ECHO or anything else
+             *
+             *
+             * But for this particular case we'll just log the information on the logs file
+            */
+
+            Log::info($this->data);
+        }
     }
 }
